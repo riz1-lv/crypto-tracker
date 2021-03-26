319d4cff48e1e67bc199f1580cb6dd7112a52ba7
riz1-lv <riz.shkh1@gmail.com>
2021-03-26 17:58:46 -0400

    added coin component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {useEffect, useState, React} from "react"
 import axios from "axios"
 import './App.css';
+import Coin from "./Coin";
 
 
 
 function App() {
 
   const [coins,setCoins] = useState([]);
+  const [search,setSearch] = useState('')
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -19,15 +21,26 @@ function App() {
     })
   },[])
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filterCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLowerCase())
+  })
 
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1>Search a currency</h1>
-        <form type="text" placeholder="Search" className="coin-input"></form>
+        <form> <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/></form>
       </div>
-
-      
+      {filterCoins.map(coin =>{
+        return (
+          <Coin key={coin.id} name ={coin.name} image={coin.image} symbol={coin.symbol}
+          volume={coin.market_cap} price={coin.current_price}/>
+        )
+      })}
     </div>
   );
 }

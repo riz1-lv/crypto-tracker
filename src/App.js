20ab5c0255ba252eeb947f6296cd76c419333bcf
riz1-lv <riz.shkh1@gmail.com>
2021-03-26 17:15:35 -0400

    added basic JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
+import {useEffect, useState, React} from "react"
+import axios from "axios"
 import './App.css';
 
+
+
 function App() {
+
+  const [coins,setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>{
+      setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
+
+
   return (
-    <div className="App">
-      <h1>API</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1>Search a currency</h1>
+        <form type="text" placeholder="Search" className="coin-input"></form>
+      </div>
+
+      
     </div>
   );
 }
